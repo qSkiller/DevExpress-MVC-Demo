9e3522bc98e49dbde9052a056c568372d651261b
Jimbo <qujiangbo@shinetechchina.com>
2016-11-28 17:18:14 +0800

    refactor codes

--- DevExpressDemo/DevExpressDemo/Controllers/AccountController.cs
+++ DevExpressDemo/DevExpressDemo/Controllers/AccountController.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Routing;
-using System.Web.Security;
 using DevExpressDemo.Models;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
